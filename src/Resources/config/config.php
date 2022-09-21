2ab87dd237fb66820156a07ec60a4b452437f428
Moritz Friedrich <moritz@matchory.com>
2022-09-21 11:18:40 +0200

    Disabled database timing by default

--- src/Resources/config/config.php
+++ src/Resources/config/config.php
@@ -13,6 +13,24 @@ return [
     */
     'enabled' => env('SERVER_TIMING_ENABLED', true),
 
-    'measure_database' => env('SERVER_TIMING_MEASURE_DATABASE', true),
+    /*
+     |--------------------------------------------------------------------------
+     | Measure total database timing
+     |--------------------------------------------------------------------------
+     |
+     | This setting, if enabled, will measure the total time spent in database
+     | queries, per connection. This value should generally safe to print
+     | in production.
+     */
+    'measure_database' => env('SERVER_TIMING_MEASURE_DATABASE', false),
+
+    /*
+     |--------------------------------------------------------------------------
+     | Measure individual query timing
+     |--------------------------------------------------------------------------
+     |
+     | This setting, if enabled, will measure the time spent for every query
+     | executed by the database, and add the timing as separate values
+     */
     'measure_queries' => env('SERVER_TIMING_MEASURE_QUERIES', false),
 ];
